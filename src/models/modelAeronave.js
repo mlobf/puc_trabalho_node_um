e0b0c4e970f555b4fbfbfb16afa84e1cd7b6d4f1
mlobf <marcoslemeborbafilho@gmail.com>
2024-09-01 00:26:55 -0300

    Refatorar Model de Aeronaves

--- src/models/modelAeronave.js
+++ src/models/modelAeronave.js
@@ -1,41 +1,113 @@
-class Aeronave {
-  /*
-     são identificadas por um prefixo (uma string).
-     Serão considerados 3 tipos de aeronave:
-    • Aeronave particular de pequeno porte;
-    • Aeronave comercial de passageiros;
-    • Aeronave comercial de carga.
+/*
+  são identificadas por um prefixo (uma string).
+  Serão considerados 3 tipos de aeronave:
+• Aeronave particular de pequeno porte;
+• Aeronave comercial de passageiros;
+• Aeronave comercial de carga.
 
-    Sobre qualquer aeronave é necessário armazenar o prefixo, 
-    o tipo de aeronave, 
-    a velocidade de cruzeiro (em quilômetros por hora) e 
-    a autonomia (em quilômetros). 
+Sobre qualquer aeronave é necessário armazenar o prefixo, 
+o tipo de aeronave, 
+a velocidade de cruzeiro (em quilômetros por hora) e 
+a autonomia (em quilômetros). 
 
-    Para as aeronaves de pequeno porte é necessário armazenar também o nome da empresa responsável pela manutenção. 
-    Para as aeronaves comerciais de passageiros é necessário armazenar a quantidade de passageiros que pode
-    transportar e para as de carga o peso máximo em toneladas que é capaz de levar. 
-    Para qualquer tipo de aeronave comercial é necessário armazenar o nome da companhia aérea.
-    Aeronaves comerciais de passageiro só podem voar acima de 28.000 pés. 
-    Aeronaves particulares de pequeno porte só podem voar entre 25.000 pés e 27.000 pés. 
-    Aeronaves de carga só podem voar entre a meia noite e as 6:00 da manhã.
-    */
-  constructor(prefixo, tipo_aeronave, velocidade_cruzeiro, autonomia) {
-    this.prefixo = prefixo;
-    this.tipo_aeronave = tipo_aeronave;
-    this.velocidade_cruzeiro = velocidade_cruzeiro;
-    this.autonomia = autonomia;
+Para as aeronaves de pequeno porte é necessário armazenar também o nome da empresa responsável pela manutenção. 
+Para as aeronaves comerciais de passageiros é necessário armazenar a quantidade de passageiros que pode
+transportar e para as de carga o peso máximo em toneladas que é capaz de levar. 
+Para qualquer tipo de aeronave comercial é necessário armazenar o nome da companhia aérea.
+Aeronaves comerciais de passageiro só podem voar acima de 28.000 pés. 
+Aeronaves particulares de pequeno porte só podem voar entre 25.000 pés e 27.000 pés. 
+Aeronaves de carga só podem voar entre a meia noite e as 6:00 da manhã.
+*/
+export class Aeronave {
+  #prefixo;
+  #velocidadeCruzeiro;
+  #autonomia;
+
+  constructor(prefixo, velocidadeCruzeiro, autonomia) {
+    this.#prefixo = prefixo;
+    this.#velocidadeCruzeiro = velocidadeCruzeiro;
+    this.#autonomia = autonomia;
   }
-  criarTipoAeronave() {
-    return "Tipo criado";
+
+  get prefixo() {
+    return this.#prefixo;
+  }
+  get velocidadeCruzeiro() {
+    return this.#velocidadeCruzeiro;
+  }
+  get autonomia() {
+    return this.#autonomia;
   }
 
-  toStr() {
-    let str_prefixo = `O prefixo da aeronave é ${this.prefixo} \n`;
-    let str_tipo = `O tipo da aeronave é ${this.tipo_aeronave} \n`;
-    let str_autonomia = `A autonomia da aeronave é ${this.autonomia} \n`;
-    let str_velocidade = `A velocidade da aeronave é ${this.velocidade_cruzeiro} \n`;
+  set prefixo(newPrefixo) {
+    this.#prefixo = newPrefixo;
+  }
 
-    return str_prefixo + str_tipo + str_autonomia + str_velocidade;
+  set velocidadeCruzeiro(newVelocidadeCruzeiro) {
+    this.#velocidadeCruzeiro = newVelocidadeCruzeiro;
+  }
+
+  set autonomia(newAutonomia) {
+    this.#autonomia = newAutonomia;
   }
 }
-export { Aeronave };
+
+export class AeronaveParticular extends Aeronave {
+  #respManutencao;
+
+  constructor(prefixo, velocidadeCruzeiro, autonomia, respManutencao) {
+    super(prefixo, velocidadeCruzeiro, autonomia);
+    this.#respManutencao = respManutencao;
+  }
+  get respManutencao() {
+    return this.#respManutencao;
+  }
+
+  set respManutencao(newRespManutencao) {
+    this.#respManutencao = newRespManutencao;
+  }
+}
+
+export class AeronaveComercial extends Aeronave {
+  #nomeCia;
+
+  constructor(prefixo, velocidadeCruzeiro, autonomia, nomeCia) {
+    super(prefixo, velocidadeCruzeiro, autonomia);
+    this.#nomeCia = nomeCia;
+  }
+  get nomeCia() {
+    return this.#nomeCia;
+  }
+
+  set nomeCia(newNomeCia) {
+    this.#nomeCia = newNomeCia;
+  }
+
+  toString() {
+    return `Aeronave de Comercial: ${this.prefixo}, velocidade de cruzeiro ${this.velocidadeCruzeiro}, autonomia ${this.autonomia}, nome da compania ${this.nomeCia}`;
+  }
+}
+
+export class AeronavePassageiros extends AeronaveComercial {
+  #maxPassageiros;
+
+  constructor(prefixo, velocidadeCruzeiro, autonomia, nomeCia, maxPassageiros) {
+    super(prefixo, velocidadeCruzeiro, autonomia, nomeCia);
+    this.#maxPassageiros = maxPassageiros;
+  }
+  get maxPassageiros() {
+    return this.#maxPassageiros;
+  }
+
+  set maxPassageiros(newMaxPassageiros) {
+    this.#maxPassageiros = newMaxPassageiros;
+  }
+  toString() {
+    return `Aeronave de Passageiros: ${this.prefixo}, velocidade de cruzeiro ${this.velocidadeCruzeiro}, autonomia ${this.autonomia}, nome da compania ${this.nomeCia}, max de passageiros ${this.#maxPassageiros}`;
+  }
+}
+
+let ap = new AeronavePassageiros("PPPae", 800, 5000, "TAM", 800);
+let ac = new AeronaveComercial("PPPae", 800, 5000, "TAM");
+console.log(ap.toString());
+console.log(ac.toString());
