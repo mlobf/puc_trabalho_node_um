35de8076cd497ab6df7e567fd81fd17bc19594ec
mlobf <marcoslemeborbafilho@gmail.com>
2024-09-01 01:17:42 -0300

    Refactor PlanoVoo

--- src/models/modelPlanoDeVoo.js
+++ src/models/modelPlanoDeVoo.js
@@ -1,32 +1,42 @@
-class PlanoDeVoo {
-  /*
-    Plano de voo
-    Sobre cada plano de voo é necessário armazenar 
-    -um identificador numérico, 
-    -a matrícula do piloto
-    -responsável por conduzir o voo, 
-    -o prefixo da aeronave que será utilizada, 
-    -a data e o horário do voo, 
-    -a aerovia escolhida, 
-    -a altitude que será usada, 
-    -a lista de slots de horário ocupados e um
-    booleano que indica se foi cancelado ou não.
+/*
+  Plano de voo
+  Sobre cada plano de voo é necessário armazenar 
+  -um identificador numérico, 
+  -a matrícula do piloto
+  -responsável por conduzir o voo, 
+  -o prefixo da aeronave que será utilizada, 
+  -a data e o horário do voo, 
+  -a aerovia escolhida, 
+  -a altitude que será usada, 
+  -a lista de slots de horário ocupados e um
+  booleano que indica se foi cancelado ou não.
 
-    validacaoPlanoVoo.
+  validacaoPlanoVoo.
 
-    Para que um plano de voo possa ser aprovado as seguintes condições tem de ser satisfeitas:
+  Para que um plano de voo possa ser aprovado as seguintes condições tem de ser satisfeitas:
 
-    • A habilitação do piloto tem de estar ativa;
-    • A aeronave tem de ter autonomia para voar o trecho (a autonomia tem de ser 10% maior que o tamanho da aerovia);
-    • A altitude escolhida tem de ser compatível com o tipo de aeronave;
-    • Não pode haver restrições de horário para o tipo de aeronave;
-    • Os slots de horário necessários têm de estar livres.
+  • A habilitação do piloto tem de estar ativa;
+  • A aeronave tem de ter autonomia para voar o trecho (a autonomia tem de ser 10% maior que o tamanho da aerovia);
+  • A altitude escolhida tem de ser compatível com o tipo de aeronave;
+  • Não pode haver restrições de horário para o tipo de aeronave;
+  • Os slots de horário necessários têm de estar livres.
 
-    Se um plano de voo é aprovado, ele recebe o identificador numérico e precisa ser registrado no
-    sistema para consulta futura. Além disso os controles relativos à ocupação das aerovias (altitudes
-    e slots de horário) têm de ser devidamente atualizados de maneira a impedir que outros planos
-    possam ser aprovados de maneira sobreposta.
+  Se um plano de voo é aprovado, ele recebe o identificador numérico e precisa ser registrado no
+  sistema para consulta futura. Além disso os controles relativos à ocupação das aerovias (altitudes
+  e slots de horário) têm de ser devidamente atualizados de maneira a impedir que outros planos
+  possam ser aprovados de maneira sobreposta.
 */
+
+export class PlanoDeVoo {
+  #id;
+  #matricPiloto;
+  #idAerovia;
+  #data;
+  #horario;
+  #altitude;
+  #slots;
+  #cancelado;
+
   constructor(
     id,
     matricPiloto,
@@ -35,43 +45,65 @@ class PlanoDeVoo {
     horario,
     altitude,
     slots,
-    idAeronave,
     cancelado
   ) {
-    this.id = id;
-    this.matriculPiloto = matriculaPiloto;
-    this.responsavelVoo = responsavelVoo;
-    this.prefixoAeronave = prefixoAeronave;
-    this.dataHoraVoo = dataHoraVoo;
-    this.idAerovia = idAerovia;
-    this.altitude = altitude;
-    this.listaSlots = listaSlots;
-    this.foiCancelado = foiCancelado;
+    this.#id = id;
+    this.#matricPiloto = matricPiloto;
+    this.#idAerovia = idAerovia;
+    this.#data = data;
+    this.#horario = horario;
+    this.#altitude = altitude;
+    this.#slots = slots;
+    this.#cancelado = cancelado;
   }
 
-  toStr() {
-    let strPlanoVoo = "Plano de voo.";
-    let strMatriculaPiloto = `Matricula do Piloto é ${this.matriculaPiloto} \n`;
-    let strResponsavelVoo = `Responsavel Voo  é ${this.responsavelVoo} \n`;
-    let strPrefixoAeronave = `Prefixo Aeronave é ${this.prefixoAeronave} \n`;
-    let strDataHoraVoo = `Data Hora Voo é ${this.dataHoraVoo} \n`;
-    let strIdAerovia = `O id da Aerovia é ${this.idAerovia} \n`;
-    let strAltitude = `Altitude é ${this.altitude} \n`;
-    let strListaSlots = `Lista Stots é ${this.listaSlots} \n`;
-    let strFoiCancelado = `Foi Cancelado é ${this.foiCancelado} \n`;
+  get id() {
+    return this.#id;
+  }
+  get matricPiloto() {
+    return this.#matricPiloto;
+  }
+  get idAerovia() {
+    return this.#idAerovia;
+  }
+  get data() {
+    return this.#data;
+  }
+  get horario() {
+    return this.#horario;
+  }
+  get altitude() {
+    return this.#altitude;
+  }
+  get slots() {
+    return this.#slots;
+  }
+  get cancelado() {
+    return this.#cancelado;
+  }
 
-    return (
-      strPlanoVoo +
-      strMatriculaPiloto +
-      strResponsavelVoo +
-      strPrefixoAeronave +
-      strDataHoraVoo +
-      strIdAerovia +
-      strAltitude +
-      strListaSlots +
-      strFoiCancelado
-    );
+  set id(newId) {
+    this.#id = newId;
+  }
+  set matricPiloto(newMatricPiloto) {
+    this.#matricPiloto = newMatricPiloto;
+  }
+  set idAerovia(newIdAerovia) {
+    this.#idAerovia = newIdAerovia;
+  }
+  set data(newData) {
+    this.#data = newData;
+  }
+  set horario(newHoraio) {
+    this.#horario = newHoraio;
+  }
+  set altitude(newAltitude) {
+    this.#altitude = newAltitude;
+  }
+  set slots(newSlots) {
+    this.#slots = newSlots;
+  }
+  set cancelado(newCancelado) {
+    this.#cancelado = newCancelado;
   }
 }
-
-export { PlanoDeVoo };
