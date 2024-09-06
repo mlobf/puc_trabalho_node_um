02a2afbe7ca39d5159948eedf2649627343be1a0
mlobf <marcoslemeborbafilho@gmail.com>
2024-09-06 01:45:24 -0300

    Serviço Piloto refatorado

--- src/models/modelsPiloto.js
+++ src/models/modelsPiloto.js
@@ -44,38 +44,3 @@ export class Piloto {
     return pn + pm + ph;
   }
 }
-
-export class ServicoPiloto {
-  constructor() {
-    this.base = [];
-  }
-  createPiloto(nome, matricula, habilitacaoAtiva) {
-    let p = new Piloto(nome, matricula, habilitacaoAtiva);
-    this.base.push(p);
-    return p;
-  }
-  getAllPiloto() {
-    return this.base;
-  }
-  getPilotoByMatricula(matricula) {
-    for (let i = 0; i < this.base.length; i++) {
-      if (this.base[i].matricula === matricula) {
-        let piloto = this.base[i];
-        return piloto;
-      }
-    }
-  }
-}
-
-let sp = new ServicoPiloto();
-
-sp.createPiloto("Marcos", 303030301, true);
-sp.createPiloto("Joao", 303030302, true);
-sp.createPiloto("Maria", 303030303, true);
-sp.createPiloto("Jose", 303030304, true);
-sp.createPiloto("Caio", 303030305, true);
-sp.createPiloto("Ze", 303030306, true);
-sp.createPiloto("Anna", 303030307, true);
-sp.createPiloto("Gabriela", 303030308, true);
-
-let pilotoMatricula = sp.getPilotoByMatricula(303030303);
--- src/models/modelsServicoPiloto.js
+++ src/models/modelsServicoPiloto.js
@@ -1,15 +1,59 @@
-class ServicoPiloto {
+import { Piloto } from "./modelsPiloto.js";
+
+export class ServicoPiloto {
   /*
     -ServicoPilotos (
         capaz de recuperar as informações relativas aos pilotos que 
         podem ser responsáveis pelos voos
     */
-  constructor(nome) {
-    this.nome = nome;
+  constructor() {
+    this.base = new Map();
   }
+
+  criarPiloto(listaDeRequisitos) {
+    const [matricula, ...resto] = listaDeRequisitos;
+    this.base.set(matricula, new Piloto(matricula, ...resto)); // Usa o identificador como chave
+  }
+
+  listarPilotos() {
+    return Array.from(this.base.values()).toString(); // Retornar todas os pilotos
+  }
+
+  recuperarPiloto(matricula) {
+    return this.base.get(matricula); // Recuperar piloto por identificador
+  }
+
+  atualizarPiloto(matricula, novosDados) {
+    if (this.base.has(matricula)) {
+      let piloto = this.base.get(matriculia);
+      Object.assign(piloto, novosDados); // Atualiza os dados da piloto
+    }
+  }
+
+  deletarPiloto(matricula) {
+    this.base.delete(matricula); // Remover piloto do Map
+  }
+
   toStr() {
     return `O nome do Serviço Piloto é ${this.nome}`;
   }
 }
 
-export { ServicoPiloto };
+// Testes
+let sp = new ServicoPiloto();
+let payload = ["marcos", "odsdifdsf3232", true];
+let payload1 = ["maria", "odsdifdsf3231", true];
+let payload2 = ["lis", "odsdifdsf3230", true];
+let payload3 = ["carolina", "odsdifdsf3239", true];
+let payload4 = ["eliza", "odsdifdsf3we2", true];
+let payload5 = ["conceicao", "odsdifddssf3232", true];
+let payload6 = ["Gisela", "odsdifdsf32sd32", true];
+
+sp.criarPiloto(payload);
+sp.criarPiloto(payload1);
+sp.criarPiloto(payload2);
+sp.criarPiloto(payload3);
+sp.criarPiloto(payload4);
+sp.criarPiloto(payload5);
+sp.criarPiloto(payload6);
+console.log(sp.listarPilotos());
